6b89b2559b025b6569b781ef1a9700f34d4c067f
shftan <shftan@gmail.com>
2018-03-05 15:11:46 -0800

    Added more data

--- process_chicago_police_ssl.R
+++ process_chicago_police_ssl.R
@@ -1,6 +1,6 @@
 ########## INSERT MICROSOFT DISCLAIMER STATEMENT AND AUTHORS HERE
 
-### Data set: Chicago Police "Strategic Subject List" Risk Score and "Party-To-Violence" Outcome
+### Data set: Chicago Police "Strategic Subject List" risk score and "Party-To-Violence" actual outcome
 ### Source: https://data.cityofchicago.org/Public-Safety/Strategic-Subject-List/4aki-r3np
 
 ########## Read data
@@ -33,11 +33,11 @@ data$parolee = factor(ifelse(is.na(data$PAROLEE_I),"N","Y"))
 data$partytoviolence = ifelse(!is.na(data$SSL_LAST_PTV_DATE),"Y","N")   
 
 ########## Select variables to keep
-## Not kept because already cleaned up into other variables: STOP_ORDER_NO, PAROLEE_I
-## Not kept because not baseline variables, would leak info into label: TRAP_STATUS, SSL_LAST_PTV_DATE, LATEST_DATE, LATEST_WEAPON_ARR_DATE, LATEST_NARCOTIC_ARR_DATE, LATEST_DOMESTIC_ARR_DATE, CPD_ARREST_I
-## Not kept because information is duplicated in other variables: AGE_GROUP, AGE_TO, AGE_CURR, RAW_SSL_SCORE, HEAT_SCORE, RAW_HEAT_SCORE
-## Not kept, because not informative (e.g. all obs have same value for this var): STATUS_I
-## Not kept, because too many missing values and imputation likely not meaningful: DOMESTIC_ARR_CNT, WEAPONS_ARR_CNT, NARCOTICS_ARR_CNT, MAJORITY_DIST, DLST, IDOC_RES_CITY, IDOC_RES_STATE_CODE, IDOC_RES_ZIP_CODE, IDOC_CPD_DIST, TRAP_FLAGS, SSL_FLAGS, LATITUDe, LONGITUDE, COMMUNITY_AREA, CENSUS_TRACT, LOCATION
+### Not kept because already cleaned up into other variables: STOP_ORDER_NO, PAROLEE_I
+### Not kept because not baseline variables, would leak info into label: TRAP_STATUS, SSL_LAST_PTV_DATE, LATEST_DATE, LATEST_WEAPON_ARR_DATE, LATEST_NARCOTIC_ARR_DATE, LATEST_DOMESTIC_ARR_DATE, CPD_ARREST_I
+### Not kept because information is duplicated in other variables: AGE_GROUP, AGE_TO, AGE_CURR, RAW_SSL_SCORE, HEAT_SCORE, RAW_HEAT_SCORE
+### Not kept, because not informative (e.g. all obs have same value for this var): STATUS_I
+### Not kept, because too many missing values and imputation likely not meaningful: DOMESTIC_ARR_CNT, WEAPONS_ARR_CNT, NARCOTICS_ARR_CNT, MAJORITY_DIST, DLST, IDOC_RES_CITY, IDOC_RES_STATE_CODE, IDOC_RES_ZIP_CODE, IDOC_CPD_DIST, TRAP_FLAGS, SSL_FLAGS, LATITUDe, LONGITUDE, COMMUNITY_AREA, CENSUS_TRACT, LOCATION
 keep = c("PREDICTOR_RAT_AGE_AT_LATEST_ARREST","PREDICTOR_RAT_VICTIM_SHOOTING_INCIDENTS","PREDICTOR_RAT_VICTIM_BATTERY_OR_ASSAULT",
          "PREDICTOR_RAT_ARRESTS_VIOLENT_OFFENSES","PREDICTOR_RAT_GANG_AFFILIATION","PREDICTOR_RAT_NARCOTIC_ARRESTS","PREDICTOR_RAT_TREND_IN_CRIMINAL_ACTIVITY",
          "PREDICTOR_RAT_UUW_ARRESTS","SEX_CODE_CD","RACE_CODE_CD","LATEST_DIST","LATEST_DIST_RES","WEAPON_I", "DRUG_I","stoporder","parolee")
@@ -47,12 +47,13 @@ data = data[,c(keep,labels)]
 data$age_last_arrest = factor(data$age_last_arrest,levels=c("less than 20","20-30","30-40","40-50","50-60","60-70","70-80"))
 
 ########## Process and output data
-## Missing data is imputed according to method specified by "impute" variable
-## Observations with missing values for labels are dropped
-## Useless variables (variable completely duplicated, or only has missing values, or only has one unique value) are dropped
-## Unused categorical levels are dropped
-## Some ML methods are sensitive to the scale of the label so large numeric labels are scaled down
+### Missing data is imputed according to method specified by "impute" variable
+### Observations with missing values for labels are dropped
+### Useless variables (variable completely duplicated, or only has missing values, or only has one unique value) are dropped
+### Unused categorical levels are dropped
+### Some ML methods are sensitive to the scale of the label so large numeric labels are scaled down
 data = process_data(data,prefix,labels,postfix,impute,truefactors)
 
-# Now you should have two csv files with the same features but one has the risk scores label and the other has the actual outcomes label.
-# Please email ht395@cornell.edu if you face problems running this script.
+
+### Now you should have two csv files with the same features but one has the risk scores label and the other has the actual outcomes label.
+### Please email ht395@cornell.edu if you face problems running this script.
---
+++ process_nypd_stopandfrisk_weapon.R
@@ -0,0 +1,86 @@
+########## INSERT MICROSOFT DISCLAIMER STATEMENT AND AUTHORS HERE
+
+### Data set: NYPD Stop and Frisk dataset with heuristic risk score from Goel et al. and Weapon Possession actual outcome
+### Source 1: https://www1.nyc.gov/site/nypd/stats/reports-analysis/stopfrisk.page
+### Source 2: https://5harad.com/data/sqf.RData (Goel's website)
+### Note that this risk score was proposed by an academic research group, not NYPD
+### References for cleaning the data set and creating the risk score:
+### - Sharad Goel, Justin M. Rao, Ravi Shroff. Precinct or Prejudice? Understanding Racial Disparities in New York City's Stop-and-Frisk Policy. Annals of Applied Statistics 2016
+### - Elaine Angelino, Nicholas Larus-Stone, Daniel Alabi, Margo Seltzer, Cynthia Rudin. Learning Certifiably Optimal Rule Lists. KDD 2017
+
+
+########## Read data
+datafile = sqf.RData
+load("raw/sqf.RData")
+prefix = "stopfrisk"
+
+# Indicate variables that will be the labels
+labels = c("heuristic","found.weapon")
+labelnames = c("score","outcome")
+
+# Indicate variables that are true categorical variables
+truefactors = "precinct"  
+
+# Indicate imputation method
+impute = "mean"
+
+# Indicate code(s) for missing data
+missing = c("", " ", "X")
+
+########## Source helper functions
+source("utils.R")
+
+########## Subset to only cpw crime (following Goel et al.) and 2009-2010
+### Risk scoring model was trained on 2009-2010, then predictions on 2011-2012 data was used in the rest of the analysis in the Goel paper)
+data = stops[stops$suspected.crime=="cpw" & (stops$year==2009 | stops$year==2010) & !is.na(stops$suspected.crime) & !is.na(stops$year),]
+
+########## Create heuristic risk score
+data$heuristic = 3*data$stopped.bc.object+1*data$stopped.bc.bulge+1*data$additional.sights
+
+########## Clean up date time information
+### Create month, day of week, time of day binned into disjoint 4 hour blocks
+library(lubridate) # for month function
+monthofyear = month(data$date)
+data$monthofyear = factor(monthofyear,levels=sort(unique(monthofyear)),labels=c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"))
+data$dayofweek = weekdays(as.Date(data$date))
+timeofday = as.numeric(hm(data$time)) 
+breaks = seq(0,24,4)*60*60
+temp = sapply(timeofday,function(x) findInterval(x,breaks))
+data$timeofday = factor(temp,levels=sort(unique(temp)),labels=c("0-4","4-8","8-12","12-16","16-20","20-24"))
+
+########## Drop variables
+### Administrative variables
+drop = c("year","serial","suspect.dob","lat","lon","xcoord","ycoord","id")
+### Variables already cleaned up into other variables
+drop = c(drop,"date","time")
+### Variables that will leak info into label
+drop = c(drop,"arrested","arrested.reason","summons.issued","frisked","searched")
+drop = c(drop,names(data)[grepl("force",names(data))])
+drop = c(drop,names(data)[grepl("frisked",names(data))])
+drop = c(drop,names(data)[grepl("searched",names(data))])
+drop = c(drop,names(data)[grepl("found",names(data))])
+### Variables with too many missing values and imputation likely not meaningful
+drop = c(drop,"officer.verbal","officer.shield","suspect.hair","stop.length")
+### Don't accidentally drop label
+drop = setdiff(drop,labels)
+data = drop_columns(data,drop)
+
+########## Drop observations missing key variables
+keep = complete.cases(data.frame(data$suspect.sex,data$suspect.race,data$suspect.weight))
+data = data[keep,]
+
+##########  Relevel ordinal variables
+data$dayofweek = factor(data$dayofweek,levels=c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"))
+data$suspect.eye = factor(data$suspect.eye,levels=c("black","blue","brown","gray","green","hazel","maroon","pink","violet","other","two different","unknown"))
+
+########## Process and output data
+### Missing data is imputed according to method specified by "impute" variable
+### Observations with missing values for labels are dropped
+### Useless variables (variable completely duplicated, or only has missing values, or only has one unique value) are dropped
+### Unused categorical levels are dropped
+### Some ML methods are sensitive to the scale of the label so large numeric labels are scaled down
+data = process_data(data,prefix,labels,postfix,impute,truefactors)
+
+
+### Now you should have two csv files with the same features but one has the risk scores label and the other has the actual outcomes label.
+### Please email ht395@cornell.edu if you face problems running this script.
