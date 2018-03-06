62c8e9bd7a16ab3d31f31b8397d3a351141fabdb
shftan <shftan@gmail.com>
2018-03-05 16:42:32 -0800

    Add Lending Club

---
+++ process_lending_club_loan.R
@@ -0,0 +1,10 @@
+########## INSERT MICROSOFT DISCLAIMER STATEMENT AND AUTHORS HERE
+
+### Data set: Lending Club dataset with risk score and Loan Default actual outcome
+### Source 1: https://www.lendingclub.com/info/download-data.action
+### Source 2: https://www.kaggle.com/wendykan/lending-club-loan-data
+
+# After discussions with Lending Club, we are updating the analysis to a newer subset 
+# of the data (after 2009) and with a slightly different outcome (12-month default, 
+# rather than end-of-term default). This file will be updated accordingly after 
+# the analysis is completed. 
