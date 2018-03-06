7acf05ee75b9df260f4543b6c07019e7bbf29058
shftan <shftan@gmail.com>
2018-03-05 16:03:54 -0800

    update distillation sketch

--- distillationsketch.r
+++ distillationsketch.r
@@ -7,12 +7,6 @@ score = ###vector_of_length_n###
 outcome = ###vector_of_length_n###
 features = ###matrix of n observations and p features###
 
-# Student model of teacher black-box risk scoring model
-scorestudentmodel = ###model_predicting_score_from_features###
-
-# Actual outcomes model
-outcomemodel = ###model_predicting_outcome_from_features###
-
 ########## Calibrate risk scores and actual outcomes
 
 ### Calculate empirical probability
@@ -37,12 +31,14 @@ plot(actualscore,actualprob)
 
 ### Plot on logit probability scale
 plot(actualscore,logit(actualprob))
-# We pick the logit scale to be the scale on which to compare the risk scores and actual outcomes models
+# We pick the logit scale to be the scale on which to compare the risk scores and 
+# actual outcomes models
 # If the relationship here is not linear, calibration is needed
 
 ### If needed, learn nonlinear transformation to calibrate
 # We use smoothing splines, and tune the smoothing parameter until monotonicity is achieved
-# This can be thought of as similar to isotonic regression but smoother because splines instead of step functions are used
+# This can be thought of as similar to isotonic regression but smoother because splines 
+# instead of step functions are used
 mgcvmodel = gam(outcome~s(score),family="binomial")
 spnow = mgcvmodel$sp
 testing = data.frame(actualscore)
@@ -55,4 +51,21 @@ tunesp = ### look at current sp value (spnow variable) and sweep a range of othe
 mgcvmodel = gam(outcome~s(score),family="binomial",sp=spnow*tunesp)
 mgcvpredlogit = predict(mgcvmodel,newdata=testing,type="link")
 
-########## Calibrate risk scores and actual outcomes
+########## Train two transparent models
+# You can use any kind of transparent model class you want as long as the model class is 
+#sufficiently powerful, which can be measured by looking at the fidelity of the 
+# student model and accuracy of the outcome model
+
+### Student model of teacher black-box risk scoring model
+scorestudentmodel = ###model_predicting_score_from_features###   
+# Note, if calibration was needed from above, the score here would be the 
+# transformed risk score
+
+### Actual outcomes model
+outcomemodel = ###model_predicting_outcome_from_features###
+
+########## Compare two models
+# This part depends on what model kind of transparent model class you have used 
+# For model classes where the prediction function is an additive form of features 
+# (e.g. logistic regresssion and GA2Ms used in the paper), you can compare
+# the 
